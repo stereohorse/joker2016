b605b7c48f905b52b78187dafa745f40c7738276
viet <tnguen@alfabank.ru>
2016-08-20 00:55:49 +0300

    feat: add evidence of bamboo reload problems

--- images/sample-api/src/main/java/ru/stereohorse/joker2016/SampleApi.java
+++ images/sample-api/src/main/java/ru/stereohorse/joker2016/SampleApi.java
@@ -1,12 +1,17 @@
 package ru.stereohorse.joker2016;
 
 
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
+import static java.lang.Thread.sleep;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.ResponseEntity.ok;
@@ -21,6 +26,16 @@ import static ru.stereohorse.joker2016.HealthStatus.UP;
 public class SampleApi {
 
     private HealthStatus appStatus = UP;
+
+    @Value("${start.delay_secs:0}")
+    private Long startDelaySecs;
+
+
+    @PostConstruct
+    @SneakyThrows
+    public void delayStart() {
+        sleep(startDelaySecs * 1000);
+    }
 
 
     @RequestMapping("/health")
